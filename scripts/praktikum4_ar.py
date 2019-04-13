ebdefb6f9d83bd9bf9496f191670bf71ac37acea
Birzhansal <mukanov98@gmail.com>
2019-04-13 15:33:35 +0300

    Cool

--- scripts/praktikum4_ar.py
+++ scripts/praktikum4_ar.py
@@ -20,21 +20,30 @@ MAX_Z_SPEED = 2
 
 
 def keep_distance(x, y, z, roll, pitch, yaw, twist):
-
+    if x>0.5:
+        twist.linear.x=0.2
+    else:
+        twist.linear.x=-0.2
     return twist
 
 # TASK 2
 
 
 def keep_center(x, y, z, roll, pitch, yaw, twist):
-
+    if y>0:
+        twist.linear.y=0.2
+    else:
+        twist.linear.y=-0.2
     return twist
 
 # TASK 3
 
 
 def turn_towards_ar(x, y, z, roll, pitch, yaw, twist):
-
+    if yaw>-1.6:
+        twist.angular.z=0.5
+    else:
+        twist.angular.z=-0.5
     return twist
 
 
@@ -64,7 +73,7 @@ def callback(data):
         rospy.loginfo("RPY: %s %s %s", roll, pitch, yaw)
 
         # Change this to your marker ID
-        if marker.id == 4:
+        if marker.id == 7:
             global cmd_vel_pub
             last_heartbeat = rospy.get_time()
 
